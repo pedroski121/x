546c7d1150d8f73363f545ec54baba3a35b80997
Pedro Obi <obipedro121@gmail.com>
2023-03-25 23:26:26 +0000

    fixing a build error on andriod - 2

--- src/pages/categories/[category]/[category-name].tsx
+++ src/pages/categories/[category]/[category-name].tsx
@@ -1,17 +1,14 @@
 import { NavBar } from "@components/navbar";
-import { BreadCrumbNav } from "@components/BreadCrumbNav";
+
 import { ProductCard } from "@components/product-card";
-import { useDynamicPath } from "@hooks/useDynamicPath";
 import { Footer } from "@components/footer";
 
 const CategoryProducts = () => {
-  const paths = useDynamicPath()
   return (
     <>
 
       <NavBar />
       <div className="container-fluid">
-        <BreadCrumbNav pages={paths} />
         <div className="row">
           <ProductCard title="Brown Shoe" price={4000} imgURL="/men/shoes.jpg" />
           <ProductCard title="White senator" price={6000} imgURL='/men/senator.jpg' />
