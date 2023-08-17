2653a12f297ca9083a3a81e4f3d628c22861f848
Pedro Obi <obipedro121@gmail.com>
2023-08-17 14:23:30 +0100

    border spinner style change for bagmodal

--- src/components/general/spinners/BorderSpinner.tsx
+++ src/components/general/spinners/BorderSpinner.tsx
@@ -4,7 +4,7 @@ type TBorderSpinner = {
     size?: boolean
 }
 const BorderSpinner: FC<TBorderSpinner> = ({ size }) => {
-    return (<div className={`d-flex justify-content-center align-items-center${size || size === undefined ? Spinners.border_spinner_sizing : ''}`}>
+    return (<div className={`d-flex justify-content-center align-items-center ${size || size === undefined ? Spinners.border_spinner_sizing : ''}`}>
         <div className="spinner-border text-dark" role="status">
             <span className="visually-hidden">Loading...</span>
         </div>
