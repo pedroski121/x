9f2badd4c5f54b7b543aee0895ffca25c298239d
Pedro Obi <obipedro121@gmail.com>
2024-04-25 13:25:45 +0100

    fix build issues on vercel

--- src/pages/checkout/index.tsx
+++ src/pages/checkout/index.tsx
@@ -7,14 +7,7 @@ const CheckOut: NextPage = () => {
 
     return <>
         <CheckoutHeader activePage="delivery" />
-        {/* <header className="d-flex flex-column flex-md-row justify-content-between py-2 px-2 border-bottom ">
-            <h1 className="fw-bold fs-6 text-secondary">DELIVERY</h1>
-            <section>
-                <span className="text-dark"> <i className="bi bi-1-circle-fill"></i> Delivery - </span>
-                <span className="text-secondary"> <i className="bi bi-2-circle-fill"></i> Review -  </span>
-                <span className="text-secondary"> <i className="bi bi-3-circle-fill"></i> Payment </span>
-            </section>
-        </header> */}
+
         <Delivery />
         <Footer />
     </>
