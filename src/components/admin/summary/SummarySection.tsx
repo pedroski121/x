0d22ac8faee5614bc34e49afc0aad397b079836c
Pedro Obi <obipedro121@gmail.com>
2024-05-05 22:51:51 +0100

    update the admin summary page to include the state of orders

--- src/components/admin/summary/SummarySection.tsx
+++ src/components/admin/summary/SummarySection.tsx
@@ -1,29 +1,60 @@
 import Link from "next/link"
 import { USER_LINKS } from "@lib/types/admin"
-import { useFetchUserPageCount } from "@hooks/admin/useFetchUserPageCount"
+import { useFetch } from "@hooks/general/useFetch"
+import { useFetchMultipleParams } from "@hooks/general/useFetchMultipleParams"
 
+type TOrderStateSummary = {
+    status: string;
+    count: number;
+}
+enum ORDERSTATE {
+    PENDING = 'pending',
+    CONFIRMED = 'confirmed',
+    SHIPPED = 'shipped',
+    DELIVERED = 'delivered'
+}
 export const SummarySection = () => {
-    const {data} = useFetchUserPageCount(30)
-    
+    const { data: userCount } = useFetch('/api/user/count')
+    const { data: productCount } = useFetch('/api/product/count')
+    const { data: orderCount } = useFetch('/api/order/count')
+
+    const params = ["pending", "confirmed", "shipped", "delivered"]
+
+
+    const { data } = useFetchMultipleParams<TOrderStateSummary>('/api/order', params)
+
+    const getDetailOfSpecificOrders = (status: string): TOrderStateSummary => {
+        return data?.find(currentStatus => currentStatus.status === status) || { status: '', count: 0 }
+    }
     return (
         <>
             <section className="row m-3">
                 <div className="col-6">
                     <div className="row flex-column gap-2 me-1">
-                        <Link href={'/admin/orders'} className="link-dark">
-                            <div className="bg-dark p-3 rounded-2">
-                                <p className="h6 text-white fw-bold">Awaiting Delivery <br /><span>0 orders</span></p>
+                        <Link href={'/admin/orders'} className="link-warning">
+                            <div className="bg-warning p-3 rounded-2">
+                                <p className="h6 text-white fw-bold">Pending Orders <br /><span>{getDetailOfSpecificOrders(ORDERSTATE.PENDING).count}</span></p>
+                            </div>
+                        </Link>
+                        <Link href={'/admin/orders'} className="link-light">
+                            <div className="bg-light p-3 rounded-2 border border-secondary border-1">
+                                <p className="h6 text-dark fw-bold">Confirmed Orders <br /><span>{getDetailOfSpecificOrders(ORDERSTATE.CONFIRMED).count}</span></p>
                             </div>
                         </Link>
                         <Link href={'/admin/orders'} className="link-secondary">
                             <div className="bg-secondary p-3 rounded-2">
-                                <p className="h6 text-white fw-bold">Successful Delivery <br /><span>0</span></p>
+                                <p className="h6 text-white fw-bold">Shipped Orders <br /><span>{getDetailOfSpecificOrders(ORDERSTATE.SHIPPED).count}</span></p>
+                            </div>
+                        </Link>
+                        <Link href={'/admin/orders'} className="link-success">
+                            <div className="bg-success p-3 rounded-2">
+                                <p className="h6 text-white fw-bold">Delivered Orders <br /><span>{getDetailOfSpecificOrders(ORDERSTATE.DELIVERED).count}</span></p>
                             </div>
                         </Link>
                     </div>
                 </div>
                 <div className="col-6 border border-dark rounded-2 d-flex  flex-column justify-content-center align-items-center">
-                    <p className="h2 text-secondary">Total Number of Sales -<span className="text-dark"> ₦0</span></p>
+                    <p className="h2 text-secondary">Total Number of Orders -<span className="text-dark"> {orderCount?.orderCount || 0}</span></p>
                 </div>
             </section>
 
@@ -32,7 +63,7 @@ export const SummarySection = () => {
                     <li className="list-group-item d-flex justify-content-between align-items-start">
                         <div className="ms-1 me-auto">
                             <div className="fw-bold">Total Number of Users</div>
-                            {data?.userCount}
+                            {userCount?.userCount || 0}
                         </div>
                     </li>
                 </Link>
@@ -40,10 +71,11 @@ export const SummarySection = () => {
                     <li className="list-group-item d-flex justify-content-between align-items-start">
                         <div className="ms-1 me-auto">
                             <div className="fw-bold">Total Number of Products</div>
-                            0
+                            {productCount?.productCount || 0}
                         </div>
                     </li>
                 </Link>
+
             </ol>
         </>
     )
